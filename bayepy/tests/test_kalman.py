973f5ab3185153bcd78a1dbfb6eb4428d915874e
Matěj Laitl <matej@laitl.cz>
2010-09-20 21:48:53 +0200

    add GNU GPL2+ copyright notices

--- bayepy/tests/test_kalman.py
+++ bayepy/tests/test_kalman.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2010 Matej Laitl <matej@laitl.cz>
+# Distributed under the terms of the GNU General Public License v2 or any
+# later version of the license, at your option.
+
 """Tests for bayepy.kalman"""
 
 import unittest as ut
--- run_tests.py
+++ run_tests.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+# Copyright (c) 2010 Matej Laitl <matej@laitl.cz>
+# Distributed under the terms of the GNU General Public License v2 or any
+# later version of the license, at your option.
+
 """Run bayepy's testsuite"""
 
 import unittest as ut
