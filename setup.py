516f8926d0b69891c22f2ec1020e7082a4ad6bf6
Matěj Laitl <matej@laitl.cz>
2010-09-23 18:52:48 +0200

    add preliminary support for building with cython

--- setup.py
+++ setup.py
@@ -3,13 +3,33 @@
 # Distributed under the terms of the GNU General Public License v2 or any
 # later version of the license, at your option.
 
-from distutils.core import setup
+from distutils.core import setup, Extension
 
-setup(name='PyBayes',
-      version='0.1-pre',
-      description='Library for convenient and fast Bayesian decision making',
-      author='Matej Laitl',
-      author_email='matej@laitl.cz',
-      url='http://github.com/strohel/PyBayes',
-      packages=['pybayes', 'pybayes.tests'],
-     )
+# TODO: enclose in try/catch
+from Cython.Distutils import build_ext
+
+cython = True
+
+params = {'name':'PyBayes',
+          'version':'0.1-pre',
+          'description':'Library for convenient and fast Bayesian decision making',
+          'author':'Matej Laitl',
+          'author_email':'matej@laitl.cz',
+          'url':'http://github.com/strohel/PyBayes',
+         }
+
+if cython:
+    params['cmdclass'] = {'build_ext': build_ext}
+    params['ext_package'] = 'pybayes'
+    params['ext_modules'] = [Extension('pdfs', ['pybayes/pdfs.py']),
+                             Extension('kalman', ['pybayes/kalman.py']),
+                             Extension('tests.test_pdfs', ['pybayes/tests/test_pdfs.py']),
+                             Extension('tests.test_kalman', ['pybayes/tests/test_kalman.py']),
+                            ]
+else:
+    print("Warning: cython was not found on your system. Falling back to pure")
+    print("         python mode which is significantly slower.")
+    params['packages'] = ['pybayes', 'pybayes.tests']
+
+
+setup(**params)
