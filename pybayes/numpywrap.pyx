1226b35d5fcbd111ff70803d968656314731297d
Matěj Laitl <matej@laitl.cz>
2010-10-12 11:50:04 +0200

    fix LAPACK dot, account for transposed matrices. (WARN: crashes sometimes)

--- pybayes/numpywrap.pyx
+++ pybayes/numpywrap.pyx
@@ -8,7 +8,7 @@
 # this file is special - it is used only in cython build, this can contain code
 # not callable from python etc.
 
-from numpy cimport int
+from numpy cimport import_array, int, PyArray_ISCARRAY_RO, PyArray_ISFARRAY_RO
 from numpy import any as np_any, array, asarray, diag, ndarray, empty
 from numpy.linalg import cholesky
 from numpy.random import normal
@@ -16,17 +16,48 @@ from numpy.random import normal
 cimport tokyo as t
 
 
+import_array()  # needed to call any PyArray functions
+
 cdef ndarray dot(ndarray a, ndarray b):
+    cdef t.CBLAS_TRANSPOSE trans_a, trans_b
+    cdef ndarray ret
+    if a is None:
+        raise TypeError("a must be numpy.ndarray")
+    if b is None:
+        raise TypeError("b must be numpy.ndarray")
+
+    if PyArray_ISCARRAY_RO(a):
+        trans_a = t.CblasNoTrans
+    elif PyArray_ISFARRAY_RO(a):
+        trans_a = t.CblasTrans
+    else:
+        raise ValueError("a must be C contiguos or F contiguos (ro)")
+
     if a.ndim == 2:
         if b.ndim == 1:
-            return t.dgemv(a, b)
+            if not PyArray_ISCARRAY_RO(b):
+                raise ValueError("b must be C Contiguos (ro) array")
+
+            ret = t.dvnewempty(a.shape[0])
+            t.dgemv6(trans_a, 1.0, a, b, 0.0, ret)
+            return ret
+
         if b.ndim == 2:
-            return t.dgemm(a, b)
+            if PyArray_ISCARRAY_RO(b):
+                trans_b = t.CblasNoTrans
+            elif PyArray_ISFARRAY_RO(b):
+                trans_b = t.CblasTrans
+            else:
+                raise ValueError("b must be C contiguos or F contiguos (ro)")
+
+            ret = t.dmnewempty(a.shape[0], b.shape[1])
+            t.dgemm7(trans_a, trans_b, 1.0, a, b, 0.0, ret)
+            return ret
     raise ValueError("I can only handle matrix*vector and matrix*matrix!")
 
 cdef ndarray inv(ndarray A):
-    p = empty((A.shape[0],), dtype=int)  # TODO: faster alt
-    R = A.copy()  # TODO: faster cython version
+    p = empty((A.shape[0],), dtype=int)
+    R = A.copy()
 
     if t.dgetrf(R, p) != 0:
         raise ValueError("A is singular or invalid argument passed (dgetrf)")
--- pybayes/tests/test_numpywrap.py
+++ pybayes/tests/test_numpywrap.py
@@ -15,20 +15,25 @@ from support import approx_eq
 class TestNumpywrap(ut.TestCase):
 
     def test_dot(self):
+        # test data
         A = array([[1., 2.], [3., -4.]])
         B = array([[2., 1.], [3.,  2.]])
         x = array([-4., -7.])
 
+        # precomputed restults
         AB = array([[ 8.,  5.], [-6., -5.]])
         BA = array([[ 5.,  0.], [ 9., -2.]])
         Ax = array([-18.,  16.])
         Bx = array([-15., -26.])
 
+        # do the check!
         for (left, right, exp) in [(A, B, AB), (B, A, BA), (A, x, Ax), (B, x, Bx)]:
             res = nw.dot(left, right)
             self.assertTrue(approx_eq(res, exp), "Arrays {0} and {1} are not fuzzy equal"
                 .format(res, exp))
 
+    def test_dot_as_in_kalman(self):
+        # a specific test that occurt in Kalman.bayes()
         A = array([[1., -0.5], [1., 0.]])
         R = array([[200., 0.], [0., 200.]])
         R = nw.dot(nw.dot(A, R), A.T)
@@ -36,7 +41,30 @@ class TestNumpywrap(ut.TestCase):
         self.assertTrue(approx_eq(R, R_exp), "{0} is not fuzzy equal {1}"
             .format(R, R_exp))
 
+    def test_dot_transposed(self):
+        # transposed optimised dot made problems in the past. Test it!
+
+        # source data
+        A = array([[1., -2.], [3., 4.]])
+        B = array([[0.5, 1.], [-1., 2.]])
+        x = array([-7., -6.])
+
+        # precomputed results
+        Atx = array([-25., -10.])
+        Atxt = array([-25., -10.])
+        AtB = array([[-2.5, 7.], [-5., 6.]])
+        ABt = array([[-1.5, -5.], [5.5, 5.]])
+        AtBt = array([[3.5, 5.], [3., 10.]])
+
+        # do the test!
+        for (left, right, exp) in [(A.T, x, Atx), (A.T, x.T, Atxt), (A.T, B, AtB),
+                                   (A, B.T, ABt), (A.T, B.T, AtBt)]:
+            res = nw.dot(left, right)
+            self.assertTrue(approx_eq(res, exp), "Arrays {0} and {1} are not fuzzy equal"
+                .format(res, exp))
+
     def test_inv(self):
+        # source data
         arrays = [
             array([[ 2.]]),
             array([[ 0.,  2.], [ 3.,  0.]]),
@@ -45,6 +73,7 @@ class TestNumpywrap(ut.TestCase):
             array([[1., 2., -3.], [1., -2., 3.], [-1., 2., 3.]])
         ]
 
+        # test that A * inv(A) = I within machine precision
         for A in arrays:
             iA = nw.inv(A)
             E = eye(A.shape[0])
